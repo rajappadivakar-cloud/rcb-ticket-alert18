7c40b230e448e705c7f02b8e4e4673fea569af46
rajappadivakar-cloud <rajappadivakar@gmail.com>
2026-03-15 22:59:20 +0530

    Update check_rcb_tickets.py

--- check_rcb_tickets.py
+++ check_rcb_tickets.py
@@ -1,10 +1,16 @@
 import os
 import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-URL = "https://shop.royalchallengers.com"
+rcb_tickets_page_url = "https://shop.royalchallengers.com/ticket"
+
+# Dates you want to monitor
+tickets_dates = ["2026-03-28", "2026-04-05"]
+
 
 def send_telegram(message):
     telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -17,33 +23,53 @@ def send_telegram(message):
     requests.post(telegram_url, data=payload)
 
 
+def get_dates_of_available_tickets(bs):
+
+    dates = []
+
+    for p in bs.find_all("p", {"class": "css-1nm99ps"}):
+        dates.append(p.text)
+
+    return dates
+
+
 def check_tickets():
 
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
-    response = requests.get(URL, headers=headers)
-    page = response.text.lower()
+    response = requests.get(rcb_tickets_page_url, headers=headers)
 
-    # Detect navigation tab
-    ticket_tab = ">tickets<" in page
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    # Detect buy button
-    buy_button = "buy tickets" in page
+    available_tickets_dates = get_dates_of_available_tickets(soup)
 
-    if ticket_tab or buy_button:
+    print("Checked at:", datetime.now())
 
-        send_telegram(
-            "🚨 RCB MATCH TICKETS ARE LIVE!\n\n"
-            "Go immediately:\n"
-            "https://shop.royalchallengers.com"
+    for available_ticket_date in available_tickets_dates:
+
+        date_obj = datetime.strptime(
+            available_ticket_date,
+            "%A, %b %d, %Y %I:%M %p"
         )
 
-        print("Ticket tab or buy button detected!")
+        formatted_date = date_obj.strftime("%Y-%m-%d")
 
-    else:
-        print("Tickets not live yet.")
+        if formatted_date in tickets_dates:
+
+            send_telegram(
+                f"🚨 RCB TICKETS AVAILABLE!\n\n"
+                f"Match Date: {formatted_date}\n"
+                f"Book now:\n"
+                f"{rcb_tickets_page_url}"
+            )
+
+            print("Tickets detected!")
+
+            return
+
+    print("Tickets not available yet")
 
 
 if __name__ == "__main__":
